9edc47122dcd39d1d21053d691fb227f24089f3d
21克的爱情 <raohong07@163.com>
2023-12-10 12:13:28 +0800

    优化

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -28,8 +28,6 @@ class _MyAppState extends State<MyApp> {
 
   // Platform messages are asynchronous, so we initialize in an async method.
   Future<void> initPlatformState() async {
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    // We also handle the message potentially returning null.
     FusionAuth.handleEvent(onEvent: (ResponseModel onEvent) async {
       if (kDebugMode) {
         print("----------------> $onEvent <----------------");
@@ -40,9 +38,6 @@ class _MyAppState extends State<MyApp> {
       // EasyLoading.show(status: onEvent['msg'] ?? "", maskType: EasyLoadingMaskType.black);
     });
     await _fusionAuthPlugin.getVersion();
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
     if (!mounted) return;
   }
 
